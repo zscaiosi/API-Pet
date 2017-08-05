51889e2dd32282b8a850635accdbe4078df31fd6
zscaiosi <zscaio.si@gmail.com>
2017-08-05 18:42:49 -0300

    Adicionando nodemailer e testando

--- src/controllers/mqttPubsController.js
+++ src/controllers/mqttPubsController.js
@@ -1,5 +1,15 @@
 let mqtt = require('mqtt');
 const fs = require('fs');
+const mailer = require('nodemailer');
+
+const nodeTransporter = mailer.createTransport({
+	service: 'gmail',
+	auth: {
+		user: 'zscaio.si@gmail.com',
+		pass: 'gm4il2911925Bcaio'
+	}
+});
+
 
 function MqttPubsController(url, t) {
 	this.url = url;
@@ -27,6 +37,23 @@ MqttPubsController.prototype.pub = function (t, message) {
 				console.log(appendErr);
 			} else {
 				console.log("Logged..."+JSON.stringify(logObj), new Date());
+			}
+		});
+
+		let recipient = "caio.saldanha@syligo.com";
+
+		let mailOptions = {
+			from: "zscaio.si@gmail.com",
+			to: recipient,
+			subject: "Nodejs TESTE",
+			html: `<h1>Olá ${recipient}!</h1><br /><p>Seu pet acaba de ser alimentado através do dispositivo que assina o tópico <strong> ${logObj.topic}</strong></p>`
+		}
+
+		nodeTransporter.sendMail(mailOptions, (mailErr, mailInfo) => {
+			if (mailErr) {
+				console.log(mailErr);
+			} else {
+				console.log('Email sent: ' + mailInfo.response);
 			}
 		});
 
